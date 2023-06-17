96bc32284f80110fd904a05fdcdae1612d60ecc9
Rafael862 <84231174+Rafael862@users.noreply.github.com>
2023-06-17 10:43:09 -0300

    Erro na aula de componentes

---
+++ rocketnotes/src/components/button/index.jsx
@@ -0,0 +1,10 @@
+import { Container } from "./styles";
+
+export function Button(){
+
+    return(
+    <Container type="button">
+        meu botão
+    </Container>
+    );
+}
--- rocketnotes/src/pages/Details/index.jsx
+++ rocketnotes/src/pages/Details/index.jsx
@@ -1,10 +1,14 @@
 import "./styles.js";
 
+import { Button } from "../../components/button";
+
 export function Details(){
   
   return(
-    <>
+    <Container>
     <h1>Hello World!</h1>
-    </>
+    <span>Rafael Ferreira</span>
+    <Button />
+    </Container>
   )
 }
