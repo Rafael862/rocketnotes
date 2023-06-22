b193798a5f6b47c314296b88ec1a12841267315a
Rafael862 <84231174+Rafael862@users.noreply.github.com>
2023-06-22 07:00:10 -0300

    Finalizado a aula de children

--- rocketnotes/src/pages/Details/index.jsx
+++ rocketnotes/src/pages/Details/index.jsx
@@ -1,14 +1,22 @@
 import "./styles.js";
-import { Container } from "./styles.js";
+import { Container, Links } from "./styles.js";
+
 
 import { Header } from "../../components/header/index.jsx";
 import { Button } from "../../components/button";
+import { Section } from "../../components/Section";
 
 export function Details(){
   
   return(
     <Container>
     <Header />
+    <Section title="Links úteis">
+    <Links>
+      <li><a href="#">http://www.rocketseat.com.br/</a></li>
+      <li><a href="#">http://www.rocketseat.com.br/</a></li>
+    </Links>
+    </Section>
     <Button title="Voltar"/>
     </Container>
   )
