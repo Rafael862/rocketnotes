e99514df7f4bc4215c616a1af3fb250ef1afa781
Rafael862 <84231174+Rafael862@users.noreply.github.com>
2023-06-21 21:18:23 -0300

    Finalizado a aula de ícones

--- rocketnotes/src/components/header/index.jsx
+++ rocketnotes/src/components/header/index.jsx
@@ -1,9 +1,23 @@
-import { Container } from "./styles";
+import { RiShutDownLine } from 'react-icons/ri';
+import { Container, Profile, Logout } from "./styles";
 
 export function Header(){
     return(
         <Container>
+            <Profile>
+                <img src="https://github.com/rafael862.png" 
+                     alt="Foto do usuário" 
+                />
 
+                <div>
+                    <span>Bem-vindo,</span>
+                    <strong>Rafael Ferreira</strong>
+                </div>
+            </Profile>
+
+            <Logout>
+                <RiShutDownLine />
+            </Logout>
         </Container>
     );
 }
