9a9c29be9b109c272adbf9b21a15b7fe60939257
Rafael862 <84231174+Rafael862@users.noreply.github.com>
2023-06-22 21:44:21 -0300

    Finalizado a aula de buttontext

--- rocketnotes/src/pages/Details/index.jsx
+++ rocketnotes/src/pages/Details/index.jsx
@@ -5,18 +5,28 @@ import { Container, Links } from "./styles.js";
 import { Header } from "../../components/header/index.jsx";
 import { Button } from "../../components/button";
 import { Section } from "../../components/Section";
+import { Tag } from "../../components/tag";
+import { ButtonText } from "../../components/ButtonText";
 
 export function Details(){
   
   return(
     <Container>
     <Header />
+
+    <ButtonText title="Excluir nota" />
     <Section title="Links úteis">
     <Links>
       <li><a href="#">http://www.rocketseat.com.br/</a></li>
       <li><a href="#">http://www.rocketseat.com.br/</a></li>
     </Links>
     </Section>
+
+    <Section title="Marcadores">
+    <Tag title="express"/>
+    <Tag title="nodejs"/>
+    </Section>
+
     <Button title="Voltar"/>
     </Container>
   )
