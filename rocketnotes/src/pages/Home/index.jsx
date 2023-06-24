c7d4936f9c4397b9b3281ac008faba44330356e9
Rafael862 <84231174+Rafael862@users.noreply.github.com>
2023-06-24 11:28:23 -0300

    Finalizado a aula de input sem bugs

--- rocketnotes/src/pages/Home/index.jsx
+++ rocketnotes/src/pages/Home/index.jsx
@@ -1,6 +1,10 @@
+import { FiPlus, FiSearch} from 'react-icons/fi';
+
 import { Container, Brand, Menu, Search, Content, NewNote } from './styles';
 
 import { Header } from '../../components/header';
+import { Input } from '../../components/Input';
+import { ButtonText } from '../../components/ButtonText';
 
 export function Home(){
     return(
@@ -15,16 +19,20 @@ export function Home(){
             </Header>
 
             <Menu>
+                <li> <ButtonText title="Todos" isActive/></li>
+                <li> <ButtonText title="React" /></li>
+                <li> <ButtonText title="Nodejs" /></li>
 
             </Menu>
             <Search>
-
+            <Input placeholder="Pesquisar pelo título" icon={FiSearch}/>
             </Search>
             <Content>
 
             </Content>
             <NewNote>
-
+                <FiPlus />
+                Criar nota
             </NewNote>
         </Container>
 
--- rocketnotes/src/pages/Home/styles.js
+++ rocketnotes/src/pages/Home/styles.js
@@ -19,22 +19,54 @@ export const Container  = styled.div`
 
 export const Brand  = styled.div`
     grid-area: brand;
-    background: red;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    border-bottom-width: 1px;
+    border-bottom: solid;
+    border-bottom-color: ${({ theme }) => theme.BACKGROUND_700};
+    
+    background-color: ${({ theme }) => theme.BACKGROUND_900};
+
+    > h1 {
+        font-size: 24px;
+        color: ${({ theme }) => theme.COLORS.ORANGE};
+    }
 `;
 
 export const Menu  = styled.ul` 
 grid-area: menu;
-background: green;`;
+background-color: ${({ theme }) => theme.BACKGROUND_900};
+padding-top: 64px;
+text-align: center;
+
+> li {
+    margin-bottom: 24px;
+}
+`;
 
 export const Search  = styled.div` 
 grid-area: search;
-background: violet;`;
+padding: 64px 64px 0;
+
+`;
 
 export const Content  = styled.div` 
 grid-area: content;
-background: blue;`;
+`;
 
 
 export const NewNote = styled.button`
  grid-area: newnote;
-    background: yellow;`;
+ background-color: ${({ theme }) => theme.COLORS.ORANGE};
+ border: none;
+
+ display: flex;
+ align-items: center;
+ justify-content: center;
+
+ svg{
+    margin-right: 8px;
+ }
+    `;
