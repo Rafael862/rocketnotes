25f0108a1ea2ba56906eba26fa1f5792737ee258
Rafael862 <84231174+Rafael862@users.noreply.github.com>
2023-06-24 10:14:30 -0300

    finalizado a aula de grid

--- rocketnotes/src/pages/Home/styles.js
+++ rocketnotes/src/pages/Home/styles.js
@@ -17,12 +17,24 @@ export const Container  = styled.div`
 
 `;
 
-export const Brand  = styled.div``;
+export const Brand  = styled.div`
+    grid-area: brand;
+    background: red;
+`;
 
-export const Menu  = styled.ul``;
+export const Menu  = styled.ul` 
+grid-area: menu;
+background: green;`;
 
-export const Search  = styled.div``;
+export const Search  = styled.div` 
+grid-area: search;
+background: violet;`;
 
-export const Content  = styled.div``;
+export const Content  = styled.div` 
+grid-area: content;
+background: blue;`;
 
-export const NewNote = styled.button``;
+
+export const NewNote = styled.button`
+ grid-area: newnote;
+    background: yellow;`;
