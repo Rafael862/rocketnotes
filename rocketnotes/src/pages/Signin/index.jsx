84a69c6a6b4db96f1c1228ebe5e354456032a97a
Rafael862 <84231174+Rafael862@users.noreply.github.com>
2023-06-27 07:04:26 -0300

    finalizado aula "formularios da signin"

--- rocketnotes/src/pages/Signin/index.jsx
+++ rocketnotes/src/pages/Signin/index.jsx
@@ -1,9 +1,40 @@
-import { Container } from "./styles";
+import { FiMail, FiLock } from 'react-icons/fi'
+
+import { Input } from '../../components/Input';
+import { Button } from '../../components/button';
+
+import { Container, Form } from "./styles";
 
 export function Signin(){
     return(
         <Container>
-            
+            <Form>
+                <h1>Rocket Notes</h1>
+                <p>Aplicação para salvar e gerenciar seus links úteis.</p>
+
+                <h2>Faça seu login</h2>
+
+                <Input
+                placeholder="E-mail"
+                type="text"
+                icon={FiMail} />
+
+                <Input
+                placeholder="Senha"
+                type="password"
+                icon={FiLock} />
+
+                <Input
+                placeholder="E-mail"
+                type="text"
+                icon={FiMail} />
+
+                <Button title="Entrar"/>
+
+                <a href="#">
+                    Criar conta
+                </a>
+            </Form>
         </Container>
     )
 }
