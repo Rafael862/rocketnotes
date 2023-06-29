1f37e4245c770ca847e4ada6872715d89d422a51
Rafael862 <84231174+Rafael862@users.noreply.github.com>
2023-06-29 20:28:37 -0300

    finalizado a aula profile

---
+++ rocketnotes/src/pages/Profile/styles.js
@@ -0,0 +1,31 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+
+    width: 100%;
+
+    > header {
+        width: 100%;
+        height: 144px;
+
+        background: ${({ theme }) => theme.COLORS.BACKGROUND_900};
+
+        display: flex;
+        align-items: center;
+        padding: 0 124px;
+
+        svg {
+        background: ${({ theme }) => theme.COLORS.BACKGROUND_100};
+        font-size: 24px;
+
+        }
+    }
+
+`;
+
+export const Form = styled.form`
+
+    max-width: 340px;
+    margin: 30px auto 0;
+
+`;
