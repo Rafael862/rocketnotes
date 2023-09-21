fadb807a2f35c78cad6792e7ade185524d902fc9
Rafael862 <84231174+Rafael862@users.noreply.github.com>
2023-09-20 21:42:42 -0300

    finalizado useeffect

--- rocketnotes/src/hooks/auth.jsx
+++ rocketnotes/src/hooks/auth.jsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useState, useEffect } from "react";
 
 import { api } from "../services/api";
 
@@ -12,8 +12,13 @@ function AuthProvider({ children }) {
         try {
             const response = await api.post("/sessions", { email, password });
             const { user, token } = response.data;
+
+            localStorage.setItem("@rocketnotes:user", JSON.stringify(user));
+            localStorage.setItem("@rocketnotes:token", token);
+
             api.defaults.headers.authorization = `Bearer ${token}`;
             setData({ user, token });
+
         } catch (error){
             if(error.response){
                 alert(error.response.data.message);
@@ -23,6 +28,20 @@ function AuthProvider({ children }) {
         }
 
     }
+
+    useEffect(() =>{
+        const token = localStorage.getItem("@rocketnotes:token");
+        const user = localStorage.getItem("@rocketnotes:user");
+
+        if(token && user) {
+            api.defaults.headers.authorization = `Bearer ${token}`;
+            setData({
+                token,
+                user: JSON.parse(user)
+            });
+        }
+    }, []);
+
     return(
         <AuthContext.Provider value={{ sigIn, user: data.user }}>
             {children}
