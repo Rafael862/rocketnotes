b1bf5fcc03c8aa4357691c95c34831b01246e5f7
Rafael862 <84231174+Rafael862@users.noreply.github.com>
2023-09-23 08:26:53 -0300

    ajuste no header

--- rocketnotes/src/hooks/auth.jsx
+++ rocketnotes/src/hooks/auth.jsx
@@ -42,7 +42,7 @@ function AuthProvider({ children }) {
         const user = localStorage.getItem("@rocketnotes:user");
 
         if(token && user) {
-            api.defaults.headers.authorization = `Bearer ${token}`;
+            api.defaults.headers.common['Authorization'] = `Bearer ${token}`;
             setData({
                 token,
                 user: JSON.parse(user)
