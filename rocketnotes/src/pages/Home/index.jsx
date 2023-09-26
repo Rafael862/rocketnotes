fab6d95af21e2d6b06fa518bc9782f8408dbf597
Rafael862 <84231174+Rafael862@users.noreply.github.com>
2023-09-26 06:59:58 -0300

    carregando tags

--- rocketnotes/src/pages/Home/index.jsx
+++ rocketnotes/src/pages/Home/index.jsx
@@ -1,5 +1,7 @@
 import { FiPlus, FiSearch} from 'react-icons/fi';
-import { Link } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+
+import { api } from '../../services/api';
 
 
 import { Container, Brand, Menu, Search, Content, NewNote } from './styles';
@@ -11,6 +13,18 @@ import { Section } from '../../components/Section';
 import { ButtonText } from '../../components/ButtonText';
 
 export function Home(){
+    const [tags, setTags] = useState([]);
+
+    useEffect(() => {
+        async function fetchTags(){
+            const response = await api.get("/tags");
+            setTags(response.data)
+        }
+        fetchTags();
+    },[]);
+
+
+
     return (
 
         <Container>
@@ -23,10 +37,20 @@ export function Home(){
             </Header>
 
             <Menu>
-                <li> <ButtonText title="Todos" isActive/></li>
-                <li> <ButtonText title="React" /></li>
-                <li> <ButtonText title="Nodejs" /></li>
-
+                <li> 
+                    <ButtonText
+                     title="Todos" 
+                     isActive/>
+                </li>
+                {
+                tags && tags.map(tag => (
+                <li key={String(tag.id)}> 
+                <ButtonText 
+                title={tag.name}
+                />
+                </li>
+                ))
+                }
             </Menu>
             <Search>
             <Input placeholder="Pesquisar pelo título" icon={FiSearch}/>
