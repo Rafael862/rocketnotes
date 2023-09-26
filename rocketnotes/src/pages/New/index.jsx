473f602b91d9e10c8beb6347fbb0e77f6912b289
Rafael862 <84231174+Rafael862@users.noreply.github.com>
2023-09-25 21:01:06 -0300

    funcionalidade de adicionar tags

--- rocketnotes/src/pages/New/index.jsx
+++ rocketnotes/src/pages/New/index.jsx
@@ -14,6 +14,9 @@ export function New(){
     const [links, setLinks] = useState([]);
     const [newLink, setNewlink] = useState("");
 
+    const [tags, setTags] = useState([]);
+    const [newTag, setNewTag] = useState("");
+
     function handleAddLink(){
         setLinks(prevState => [...prevState, newLink]);
         setNewlink("");
@@ -21,6 +24,11 @@ export function New(){
 
     function handleRemoveLink(deleted){
         setLinks(prevState => prevState.filter(link => link !== deleted));
+    }
+
+    function handleAddTag(){
+        setTags(prevState => [...prevState, newTag]);
+        setNewTag("");
     }
 
     return(
@@ -57,8 +65,23 @@ export function New(){
 
                     <Section title="Marcadores">
                         <div className='tags'>
-                            <NoteItem value="React"/>
-                            <NoteItem isNew placeholder="Nova tag"/>
+                            {
+                                tags.map((tag, index) =>(
+                                <NoteItem 
+                                key={String(index)}
+                                value={tag}
+                                onClick={() => { }}
+                                />
+                                ))
+                            }
+
+                            <NoteItem 
+                            isNew  
+                            placeholder="Nova tag"
+                            onChange={e => setNewTag(e.target.value)}
+                            value={newTag}
+                            onClick={handleAddTag}
+                            />
                         </div>
                     </Section>
 
