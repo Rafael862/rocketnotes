b0f609aecf54ccbe7076c77704aef1a5d75f13b5
Rafael862 <84231174+Rafael862@users.noreply.github.com>
2023-09-26 21:09:54 -0300

    carregando notas

--- rocketnotes/src/pages/Home/index.jsx
+++ rocketnotes/src/pages/Home/index.jsx
@@ -13,8 +13,10 @@ import { Section } from '../../components/Section';
 import { ButtonText } from '../../components/ButtonText';
 
 export function Home(){
+    const [search, setSearch] = useState("");
     const [tags, setTags] = useState([]);
     const [tagsSelected, setTagsSelected] = useState([]);
+    const [notes, setNotes] = useState([]);
 
     function handleTagSelected(tagName){
         const alreadySelected = tagsSelected.includes(tagName);
@@ -37,7 +39,15 @@ export function Home(){
         fetchTags();
     },[]);
 
+    useEffect(() => {
+        async function fetchNotes(){
+            const response = await api.get(`/notes?title=${search}&tags=${tagsSelected}`);
+            setNotes(response.data);
+        }
 
+        fetchNotes();
+
+    },[tagsSelected, search]);
 
     return (
 
@@ -70,19 +80,25 @@ export function Home(){
                 ))
                 }
             </Menu>
+
             <Search>
-            <Input placeholder="Pesquisar pelo título" icon={FiSearch}/>
+            <Input 
+            placeholder="Pesquisar pelo título" 
+            icon={FiSearch}
+            onChange={() => setSearch(e.target.value)}
+            />
             </Search>
+
             <Content>
                 <Section title="Minhas notas">
-                    <Note data={{
-                        title: 'React', 
-                        tags:[
-                            {id: '1', name:'react'},
-                            {id: '2', name:'rocketseat'},
-                        ]
-                        }}
-                        />
+                    {
+                        notes.map(note => (
+                            <Note 
+                            key={String(note.id)}
+                            data={note}
+                            />
+                        ))
+                    }
                 </Section>
 
             </Content>
