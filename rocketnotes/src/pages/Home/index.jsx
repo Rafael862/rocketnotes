3a501004cc0d72176cf3f77e03358f9d71fa6a14
Rafael862 <84231174+Rafael862@users.noreply.github.com>
2023-09-26 21:11:42 -0300

    filtrar notas

--- rocketnotes/src/pages/Home/index.jsx
+++ rocketnotes/src/pages/Home/index.jsx
@@ -19,6 +19,10 @@ export function Home(){
     const [notes, setNotes] = useState([]);
 
     function handleTagSelected(tagName){
+        if(tagName === "all"){
+            return setTagsSelected([]);
+        }
+
         const alreadySelected = tagsSelected.includes(tagName);
         
         if(alreadySelected){
