1ad9314bdab010e89294d1cecd56aa17b5c6a6d5
Rafael862 <84231174+Rafael862@users.noreply.github.com>
2023-10-01 10:13:12 -0300

    Redirecionamento do logout, terminado projeto.

--- rocketnotes/src/components/header/index.jsx
+++ rocketnotes/src/components/header/index.jsx
@@ -1,12 +1,19 @@
 import { RiShutDownLine } from 'react-icons/ri';
 import {useAuth} from "../../hooks/auth";
 import { api } from '../../services/api';
+import { useNavigate } from 'react-router-dom';
 
 import { Container, Profile, Logout } from "./styles";
 
 export function Header(){
-
     const { signOut, user } = useAuth();
+    const navigation = useNavigate();
+
+    function handleSignOut(){
+        navigation("/");
+        signOut();
+    }
+
     const avatarUrl = user.avatar ? `${api.defaults.baseURL}/files/${user.avatar}` : avatarPlaceholder;
 
     return(
@@ -22,7 +29,7 @@ export function Header(){
                 </div>
             </Profile>
 
-            <Logout onClick={signOut}>
+            <Logout onClick={handleSignOut}>
                 <RiShutDownLine />
             </Logout>
         </Container>
